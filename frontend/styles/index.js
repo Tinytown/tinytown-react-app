3ee080c519502845027641bf4102b7634ab32e3a
Carlton <carlton.keedy@gmail.com>
2020-09-02 08:37:25 -0700

    Add files to specify common colors and fonts

---
+++ frontend/styles/index.js
@@ -0,0 +1,7 @@
+import * as Color from './color';
+import * as Typography from './typography';
+
+export {
+	Color,
+	Typography
+}
---
+++ frontend/styles/typography.js
@@ -0,0 +1,7 @@
+export const subheader1 = {
+	fontFamily: 'Nunito',
+	fontStyle: 'normal',
+	fontWeight: 'bold',
+	fontSize: '32px',
+	lineHeight: '40px',
+};
