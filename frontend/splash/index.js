b161e7d253abc7b04f27cf7c646fb8ec29573d30
Hami Abdi <abdih@users.noreply.github.com>
2020-09-05 12:06:06 -0700

    Fix Package Lock Files and Improve Style of Temporary Page with Twitter Authorization Button

--- frontend/splash/index.js
+++ frontend/splash/index.js
@@ -49,7 +49,9 @@ export default class Splash extends Component {
 const styles = StyleSheet.create({
   landscape: {
     backgroundColor: '#000000',
-    height: '100%'
+    height: '100%',
+    alignItems: 'center',	
+    justifyContent: 'center'
   },
   twitterButton: {
     backgroundColor: 'gray',
