1e6d73baea638b10b9168139751a32d694ff3d41
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2020-10-11 16:03:49 -0700

    i36: Add index.js to library/components folder

---
+++ src/library/components/index.js
@@ -0,0 +1,10 @@
+// Index for all Components: import { AppBar, FAB } from 'library/components' 
+
+import { default as AppBar }  from './AppBar';
+import { default as FAB }  from './FAB';
+
+
+export {
+  AppBar,
+  FAB
+}
