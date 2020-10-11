7631887685966003cb260778ce546a995690ea20
Carlton <carlton.keedy@gmail.com>
2020-10-10 17:00:26 -0700

    Add SVGs for info and sign out
    I have no idea whether I did this right 😅

--- frontend/assets/svg/index.js
+++ frontend/assets/svg/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PlaceholderIcon from './ic_placeholder_24dp'
+import InfoIcon from './ic_info_24dp'
 import CrosshairsIcon from './ic_crosshairs_24dp'
 import MegaphoneIcon from './ic_megaphone_24dp'
+import SignOutIcon from './ic_signout_24dp'
 import TwitterIcon from './ic_twitter_24dp'
 
 
@@ -9,8 +11,12 @@ const Icon = props => {
   switch(props.icon) {
     case "crosshairs":
       return <CrosshairsIcon {...props} />;
+    case "info":
+      return <InfoIcon {...props} />;
     case "megaphone":
       return <MegaphoneIcon {...props} />;
+    case "signout":
+      return <SignOutIcon {...props} />;
     case "twitter":
       return <TwitterIcon {...props} />;
     default:
