9b0c75f88dd6ac842ac85b1a685e88b957ffd970
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2020-10-25 15:57:58 -0700

    i36: Refactor ratio const in normalize.js

--- src/library/utils/normalize.js
+++ src/library/utils/normalize.js
@@ -1,22 +1,18 @@
-import { values } from 'lodash';
+import * as R from 'ramda';
+const {cond, T: otherwise, identity, always} = R;
 import { PixelRatio, Dimensions } from 'react-native';
 import { StyleSheet } from 'react-native';
 
 
-let ratio = PixelRatio.get();
-if (ratio > 0 && ratio <= 0.75) {
-  ratio = 0.75;
-} else if (ratio > 0.75 && ratio <= 1) {
-  ratio = 1;
-} else if (ratio > 1 && ratio <= 1.5) {
-  ratio = 1.5;
-} else if (ratio > 1.5 && ratio <= 2) {
-  ratio = 2;
-} else if (ratio > 2 && ratio <= 3) {
-  ratio = 3;
-} else if (ratio > 3) {
-  ratio = 4;
-}
+const ratio = cond([
+  [r => r > 0 && r <= 0.75, always(0.75)],
+  [r => r > 0.75 && r <= 1, always(1)],
+  [r => r > 1 && r <= 1.5, always(1.5)],
+  [r => r > 1.5 && r <= 2, always(2)],
+  [r => r > 2 && r <= 3, always(3)],
+  [r => r > 3, always(4)],
+  [otherwise, identity]
+])(PixelRatio.get());
 
 
 const normalize = (size) => {
