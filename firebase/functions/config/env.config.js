fc8866684df9dbb805278dbfa5aafb2694bccd73
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2021-01-27 17:25:50 -0800

    i60: Add firebase project files and config

---
+++ firebase/functions/config/env.config.js
@@ -0,0 +1,10 @@
+const functions = require("firebase-functions");
+
+// use firebase config when deployed to firebase
+const deployedToFirebase = process.env.NODE_ENV === 'prod';
+if (deployedToFirebase) {
+  module.exports = functions.config().env;
+} else {
+  module.exports = require('../../../react-app/src/config/env.config');
+}
+
