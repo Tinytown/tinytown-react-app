aa050a7e06073b362877c551d2a1e3b7dbd16a91
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2021-02-01 14:34:12 -0800

    i63: Migrate database to firestore

--- firebase/functions/shouts/createShout.js
+++ firebase/functions/shouts/createShout.js
@@ -1,10 +1,12 @@
 /* eslint-disable camelcase*/
 const admin = require('firebase-admin');
-const OpenLocationCode = require('../library/openlocationcode');
+const { encode, decode } = require('pluscodes');
 
 module.exports = async (data, context) => {
+  CODE_PRECISION = 6;
   const { text, sourcePlatform, coordinates  } = data;
-  const { auth } = context;
+  const { auth: { uid, token: { name } } } = context;
+  const db = admin.firestore();
 
   const shout = {
     created_at: Date.now(),
@@ -14,22 +16,26 @@ module.exports = async (data, context) => {
   };
 
   const user = {
-    uid: auth.uid,
-    name: auth.token.name,
+    uid,
+    name,
   };
 
+  const plusCode = encode({ longitude: coordinates[0], latitude: coordinates[1] }, CODE_PRECISION);
+  const area = decode(plusCode);
+
+  const shoutRef =  db.collection('shouts').doc();
+  const mapRef =  db.collection('map').doc(plusCode);
+  const userRef =  db.collection('users').doc(uid);
+  shout.id = shoutRef.id;
+
   try {
-    const plusCode = OpenLocationCode.encode(coordinates[1], coordinates[0], OpenLocationCode.CODE_PRECISION_WIDE);
-
-    const shoutRef = admin.database().ref(`/shouts/${plusCode}`)
-      .push();
-    shout.id = shoutRef.key;
     await shoutRef.set({ ...shout, user });
+    await mapRef.collection('shouts').doc(shoutRef.id)
+      .set({ ...shout });
+    mapRef.set({ area });
+    userRef.collection('shouts').add(shout);
 
-    await admin.database().ref(`/users/${auth.uid}/shouts/${shoutRef.key}`)
-      .set(shout);
-
-    return { shoutId: shoutRef.key };
+    return { shoutId: shoutRef.id };
   } catch (error) {
     console.log(error);
     return;
--- react-app/src/library/hooks/useAppLaunch.js
+++ react-app/src/library/hooks/useAppLaunch.js
@@ -3,6 +3,7 @@ import { AppState } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import auth from '@react-native-firebase/auth';
 import functions from '@react-native-firebase/functions';
+import firestore from '@react-native-firebase/firestore';
 import { useDispatch } from 'react-redux';
 import { updateAppState, getStateFromLS } from 'rdx/appState';
 import { signIn, updateAuth } from 'rdx/authState';
@@ -16,6 +17,7 @@ export default (isSignedIn) => {
   useEffect(() => {
     if (config.NODE_ENV === 'dev') {
       functions().useFunctionsEmulator('http://localhost:5001');
+      firestore().settings({ host: 'localhost:8080', ssl: false });
     }
 
     dispatch(getStateFromLS());
