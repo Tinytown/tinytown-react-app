a770870c71ab9f14ebbe91e64f5beac772d972ec
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2021-02-09 18:20:37 -0800

    i38: Update firebase deploy to use Doppler

--- firebase/functions/config/env.config.js
+++ firebase/functions/config/env.config.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 
 // Use firebase config when deployed to firebase
-const deployedToFirebase = process.env.NODE_ENV === 'prod';
+const deployedToFirebase = process.env.NODE_ENV !== 'dev';
 if (deployedToFirebase) {
   module.exports = functions.config().env;
 } else {
