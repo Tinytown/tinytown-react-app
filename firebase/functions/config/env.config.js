b01391ca133c00dd8baf3fcae2e0ba72042c3e76
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2021-02-23 18:14:40 -0800

    Hotfix: Remove doppler token

--- firebase/functions/config/env.config.js
+++
@@ -1,8 +0,0 @@
-const config = Object.freeze({
-  DATABASE_URL: process.env.DATABASE_URL,
-  TWITTER_API_KEY: process.env.TWITTER_API_KEY,
-  TWITTER_API_SECRET: process.env.TWITTER_API_SECRET,
-  DEV_TOKEN: 'dp.st.dev.yKYSnfkW0sUgGbjnMFyUaOcUNW3KUyLvzjCndC5q',
-});
-
-module.exports = config;
